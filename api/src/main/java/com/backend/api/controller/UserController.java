c08912b53b89e289f83e8a7f3e1a1b3405ec9a20
MayerlyAraque <maraqued@ufpso.edu.co>
2025-03-06 20:19:54 -0500

    API-SpringBoot

---
+++ api/src/main/java/com/backend/api/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.backend.api.controller;
+
+import com.backend.api.model.User;
+import com.backend.api.service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/users")
+@CrossOrigin(origins = "*") 
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<User> getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+}
---
+++ api/src/main/java/com/backend/api/model/User.java
@@ -0,0 +1,32 @@
+package com.backend.api.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String cellNumber;
+
+    @Column(nullable = false)
+    private String password;
+
+    
+}
