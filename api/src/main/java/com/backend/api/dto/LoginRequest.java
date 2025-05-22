7f7e59f8dbb4eff352efcb5830b08f8a0bf671d2
verosb <vbsanchezb@ufpso.edu.co>
2025-05-22 11:59:26 -0500

    Reset password e integración de firebase con Google

--- api/src/main/java/com/backend/api/dto/LoginRequest.java
+++ api/src/main/java/com/backend/api/dto/LoginRequest.java
@@ -1,5 +1,6 @@
 package com.backend.api.dto;
 
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,5 +16,21 @@ public class LoginRequest {
                 "email='" + email + '\'' +
                 ", password='" + password + '\'' +
                 '}';
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
